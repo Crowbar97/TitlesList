868dee68b7537358ff89c8388d8b2c556069b494
Stokolesov Maxim <mr.max-7@yandex.ru>
2018-07-05 14:02:09 -0700

    Feature fixes
    - "Go" button click and input text on change events moved from Store to SearchPanel component;
    - Added "Go" button ability to be disabled.

--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import styles from "./styles.module.css";
 import store from "../../store/index";
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
+@observer
 class SearchPanel extends Component {
+    @observable filter = "";
+
+    constructor(props) {
+        super(props);
+        this.refreshFilter = this.refreshFilter.bind(this);
+        this.fetchItems = this.fetchItems.bind(this);
+    }
+
+    refreshFilter(event) {
+        this.filter = event.target.value;
+    }
+
+    fetchItems(event) {
+        store.fetchItems(this.filter);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className={styles.block}>
-                <form onSubmit={store.fetchItems}>
-                    <input type="text" onChange={store.refreshFilter} className={styles.input} placeholder="Search"/>
-                    <input type="submit" value="Go" className={styles.button} />
+                <form onSubmit={this.fetchItems}>
+                    <input type="text" onChange={this.refreshFilter} className={styles.input} placeholder="Search"/>
+                    <input type="submit" value="Go" disabled={this.filter===""} className={styles.button} />
                 </form>
             </div>
         );
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,27 @@
 import { observable } from 'mobx';
 
 class Store {
-
     @observable itemList = [{title: 1, text: 1, key: 1}, {title: 2, text: 2, key: 2}];
 
     constructor() {
-        this.filter = "";
-        this.refreshFilter = this.refreshFilter.bind(this);
         this.fetchItems = this.fetchItems.bind(this);
-        this.fetchItems();
       }
 
-    refreshFilter(event) {
-        this.filter = event.target.value;
+    getUrlByFilter(filter) {
+        return "https://chroniclingamerica.loc.gov/search/titles/results/?terms=" + filter + "&format=json&page=1";
     }
 
-    getUrlByFilter() {
-        return "https://chroniclingamerica.loc.gov/search/titles/results/?terms=" + this.filter + "&format=json&page=1";
-    }
-
-    fetchItems(event) {
-        console.log("Fetching data with filter \"" + this.filter + "\"...\n");
-        fetch(this.getUrlByFilter())
-        .then(
-            response => response.json()
-        )
-        .then(
-            jsonArray => this.itemList = jsonArray.items.map(item => ({title: item.title, text: item.place_of_publication, key: item.id}))
-        );
-
-        if (event !== undefined)
-            event.preventDefault();
+    fetchItems(filter) {
+        if (filter !== undefined) {
+            console.log("Fetching data with filter \"" + filter + "\"...\n");
+            fetch(this.getUrlByFilter(filter))
+            .then(
+                response => response.json()
+            )
+            .then(
+                jsonArray => this.itemList = jsonArray.items.map(item => ({title: item.title, text: item.place_of_publication, key: item.id}))
+            );
+        }
     }
 }
 
