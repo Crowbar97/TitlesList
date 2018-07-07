d740c4eccdef3f0c034ac368ff33b001f405931a
Stokolesov Maxim <mr.max-7@yandex.ru>
2018-07-06 17:31:27 -0700

    added fetch asynchrony

--- src/store/index.js
+++ src/store/index.js
@@ -8,26 +8,20 @@ class Store {
     }
 
     getUrlByFilter(filter) {
-        return "https://chroniclingamerica.loc.gov/search/titles/results/?terms=" + filter + "&format=json&page=1";
+        return `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${filter}&format=json&page=1`;
     }
 
-    @action fetchItems(filter) {
-        console.log("Fetching data with filter \"" + filter + "\"...\n");
-        fetch(this.getUrlByFilter(filter))
-            .then(
-                response => response.json(),
-                error => console.log("Fetch error: " + error + "\n")
-            )
-            .then(
-                jsonArray => {
-                    if (jsonArray !== undefined) {
-                        this.itemList = jsonArray.items.map(item => ({ title: item.title, text: item.place_of_publication, key: item.id }));
-                        console.log(this.itemList.length + " item(s) were fetched.\n");
-                    }
-                },
-                error => console.log("JSON parsing error: " + error + "\n")
-            );
-        console.log("Fetch request was successfully sended!\n");
+    @action async fetchItems(filter) {
+        try {
+            console.log("Fetching data with filter \"" + filter + "\"...\n");
+            const response = await fetch(this.getUrlByFilter(filter));
+            const jsonData = await response.json();
+            this.itemList = jsonData.items.map(item => ({ title: item.title, text: item.place_of_publication, key: item.id }));
+            console.log(this.itemList.length + " item(s) were fetched.\n");
+        }
+        catch (e) {
+            console.log("Error " + e.name + ": " + e.message + "\n" + e.stack);
+        }
     }
 }
 
