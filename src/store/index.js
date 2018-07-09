eb65e7c0415ec9daacc6cd4d80fe4546386dd655
Stokolesov Maxim <mr.max-7@yandex.ru>
2018-07-09 09:11:32 -0700

    added json-server

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,7 @@ class Store {
     @observable itemList = [];
 
     getUrlByFilter(filter) {
-        return `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${filter}&format=json&page=1`;
+        return `http://localhost:3000/items?q=${filter}`;
     }
 
     @action async fetchItems(filter) {
@@ -24,7 +24,7 @@ class Store {
             console.log(`Fetching data with filter "${filter}"...\n`);
             const response = await fetch(this.getUrlByFilter(filter));
             const jsonData = await response.json();
-            this.itemList = jsonData.items.map((item, index) => (new Item(item.title, item.place_of_publication, index)));
+            this.itemList = jsonData.map((item, index) => (new Item(item.title, item.text, index)));
             console.log(`${this.itemList.length} item(s) were fetched.\n`);
         }
         catch (e) {
